ff46656bd8467896c3e7c29422f94f6ca62de026
RESL <taoyao@usc.edu>
2021-03-18 13:50:08 -0700

    Add force control experiment

--- experiments/echo_robot_state.cpp
+++ experiments/echo_robot_state.cpp
@@ -94,7 +94,7 @@ int main(int argc, char **argv)
       std::this_thread::sleep_for(50ms);
     }
 
-    std::ofstream file("log.json");
+    std::ofstream file("log_panda_nop.json");
     file << log;
 
     std::cout << "Done." << std::endl;
