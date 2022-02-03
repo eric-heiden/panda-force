9933f15334d0b1682fdc323bca50dee6c54397a3
Eric Heiden <eheiden@nvidia.com>
2022-02-03 12:25:16 -0800

    Load EE settings from JSON config

--- experiments/echo_robot_state.cpp
+++ experiments/echo_robot_state.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <thread>
 
 #include <fstream>
@@ -7,44 +7,46 @@
 #include <Eigen/Dense>
 
 #include <franka/exception.h>
-#include <franka/robot.h>
 #include <franka/model.h>
+#include <franka/robot.h>
 
 #include "state_recorder.hpp"
 
+#include "settings.h"
+
 template <typename Scalar, std::size_t Size>
-void print(const std::array<Scalar, Size> &a)
-{
-  for (size_t i = 0; i < Size; ++i)
-  {
+void print(const std::array<Scalar, Size> &a) {
+  for (size_t i = 0; i < Size; ++i) {
     std::cout << a[i];
-    if (i < Size - 1)
-    {
+    if (i < Size - 1) {
       std::cout << ", ";
     }
   }
   std::cout << std::endl;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   using namespace std::chrono_literals;
+
+  std::cout << "PANDA_HOME: " << PANDA_HOME << std::endl;
 
   // if (argc != 2) {
   //   std::cerr << "Usage: " << argv[0] << " <robot-hostname>" << std::endl;
   //   return -1;
   // }
 
-  try
-  {
+  try {
     // franka::Robot robot(argv[1]);
     std::string franka_address = "172.16.0.2";
     franka::Robot robot(franka_address);
+    settings::set_EE_properties(
+        robot,
+        std::string(PANDA_HOME) +
+            std::string(
+                "/edc-knife-endeffector-config.json"));
 
     // load the kinematics and dynamics model
     franka::Model model = robot.loadModel();
-
-    
 
     // franka::RobotState initial_state = robot.readOnce();
     // std::array<double, 7> init_gravity_array = model.gravity(initial_state);
@@ -52,8 +54,7 @@ int main(int argc, char **argv)
     StateRecorder state_recorder("log_panda_nop.json", model);
     state_recorder.print_to_stdout = true;
 
-    for (int i = 0; i < 200; ++i)
-    {
+    for (int i = 0; i < 200; ++i) {
       robot.read(state_recorder);
       std::this_thread::sleep_for(50ms);
     }
@@ -61,9 +62,7 @@ int main(int argc, char **argv)
     state_recorder.save();
 
     std::cout << "Done." << std::endl;
-  }
-  catch (franka::Exception const &e)
-  {
+  } catch (franka::Exception const &e) {
     std::cerr << "ERROR: " << e.what() << std::endl;
     return EXIT_FAILURE;
   }
--- experiments/settings.h
+++ experiments/settings.h
@@ -1,61 +1,106 @@
 #pragma once
 
-#include <string>
+#include "json.hpp"
 #include <franka/robot.h>
+#include <string>
 
-namespace settings
-{
-    /**
-     * Frequency at which the log is populated with robot state information.
-     * If it is too high, libfranka will show the following reflex error:
-     *  
-     * libfranka: Move command aborted: motion aborted by reflex! ["communication_constraints_violation"]
-     */
-    static const double kLogRate = 200.0;
+namespace settings {
+/**
+ * Frequency at which the log is populated with robot state information.
+ * If it is too high, libfranka will show the following reflex error:
+ *
+ * libfranka: Move command aborted: motion aborted by reflex!
+ * ["communication_constraints_violation"]
+ */
+static const double kLogRate = 200.0;
 
-    static const std::string franka_address = "172.16.0.2";
+static const std::string franka_address = "172.16.0.2";
 
-    void set_robot_parameters(franka::Robot &robot)
-    {
-        // Set additional parameters always before the control loop, NEVER in the control loop!
-        // Set the joint impedance.
-        robot.setJointImpedance({{8000, 8000, 8000, 7000, 7000, 6500, 6500}});
+void set_robot_parameters(franka::Robot &robot) {
+  // Set additional parameters always before the control loop, NEVER in the
+  // control loop! Set the joint impedance.
+  robot.setJointImpedance({{8000, 8000, 8000, 7000, 7000, 6500, 6500}});
 
-        // Set the collision behavior.
-        std::array<double, 7> lower_torque_thresholds_nominal{
-            {25.0, 25.0, 22.0, 20.0, 19.0, 17.0, 14.}};
-        std::array<double, 7> upper_torque_thresholds_nominal{
-            {35.0, 35.0, 32.0, 30.0, 29.0, 27.0, 24.0}};
-        std::array<double, 7> lower_torque_thresholds_acceleration{
-            {25.0, 25.0, 22.0, 20.0, 19.0, 17.0, 14.0}};
-        std::array<double, 7> upper_torque_thresholds_acceleration{
-            {35.0, 35.0, 32.0, 30.0, 29.0, 27.0, 24.0}};
-        std::array<double, 6> lower_force_thresholds_nominal{{160.0, 160.0, 160.0, 150.0, 150.0, 150.0}};
-        std::array<double, 6> upper_force_thresholds_nominal{{180.0, 180.0, 180.0, 170.0, 170.0, 170.0}};
-        std::array<double, 6> lower_force_thresholds_acceleration{{160.0, 160.0, 160.0, 150.0, 150.0, 150.0}};
-        std::array<double, 6> upper_force_thresholds_acceleration{{180.0, 180.0, 180.0, 170.0, 170.0, 170.0}};
-        robot.setCollisionBehavior(
-            lower_torque_thresholds_acceleration, upper_torque_thresholds_acceleration,
-            lower_torque_thresholds_nominal, upper_torque_thresholds_nominal,
-            lower_force_thresholds_acceleration, upper_force_thresholds_acceleration,
-            lower_force_thresholds_nominal, upper_force_thresholds_nominal);
-    }
-
-    /**
-     * Sets a default collision behavior, joint impedance, Cartesian impedance, and filter frequency.
-     * Taken from libfranka examples.
-     *
-     * @param[in] robot Robot instance to set behavior on.
-     */
-    void set_default_behavior(franka::Robot &robot)
-    {
-        robot.setCollisionBehavior(
-            {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}}, {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
-            {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}}, {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
-            {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}}, {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
-            {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}}, {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}});
-        robot.setJointImpedance({{3000, 3000, 3000, 2500, 2500, 2000, 2000}});
-        // these are the highest possible Cartesian impedance settings!
-        robot.setCartesianImpedance({{3000, 3000, 3000, 300, 300, 300}});
-    }
+  // Set the collision behavior.
+  std::array<double, 7> lower_torque_thresholds_nominal{
+      {25.0, 25.0, 22.0, 20.0, 19.0, 17.0, 14.}};
+  std::array<double, 7> upper_torque_thresholds_nominal{
+      {35.0, 35.0, 32.0, 30.0, 29.0, 27.0, 24.0}};
+  std::array<double, 7> lower_torque_thresholds_acceleration{
+      {25.0, 25.0, 22.0, 20.0, 19.0, 17.0, 14.0}};
+  std::array<double, 7> upper_torque_thresholds_acceleration{
+      {35.0, 35.0, 32.0, 30.0, 29.0, 27.0, 24.0}};
+  std::array<double, 6> lower_force_thresholds_nominal{
+      {160.0, 160.0, 160.0, 150.0, 150.0, 150.0}};
+  std::array<double, 6> upper_force_thresholds_nominal{
+      {180.0, 180.0, 180.0, 170.0, 170.0, 170.0}};
+  std::array<double, 6> lower_force_thresholds_acceleration{
+      {160.0, 160.0, 160.0, 150.0, 150.0, 150.0}};
+  std::array<double, 6> upper_force_thresholds_acceleration{
+      {180.0, 180.0, 180.0, 170.0, 170.0, 170.0}};
+  robot.setCollisionBehavior(
+      lower_torque_thresholds_acceleration,
+      upper_torque_thresholds_acceleration, lower_torque_thresholds_nominal,
+      upper_torque_thresholds_nominal, lower_force_thresholds_acceleration,
+      upper_force_thresholds_acceleration, lower_force_thresholds_nominal,
+      upper_force_thresholds_nominal);
 }
+
+/**
+ * Sets a default collision behavior, joint impedance, Cartesian impedance, and
+ * filter frequency. Taken from libfranka examples.
+ *
+ * @param[in] robot Robot instance to set behavior on.
+ */
+void set_default_behavior(franka::Robot &robot) {
+  robot.setCollisionBehavior({{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+                             {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+                             {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
+                             {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
+                             {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+                             {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+                             {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
+                             {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}});
+  robot.setJointImpedance({{3000, 3000, 3000, 2500, 2500, 2000, 2000}});
+  // these are the highest possible Cartesian impedance settings!
+  robot.setCartesianImpedance({{3000, 3000, 3000, 300, 300, 300}});
+}
+
+void set_EE_properties(franka::Robot &robot,
+                       const std::string &config_filename) {
+  std::ifstream config_file(config_filename);
+  nlohmann::json config = nlohmann::json::parse(config_file);
+  std::cout << "Setting EE properties from " << config_filename << std::endl;
+  double mass = config["mass"];
+  std::array<double, 3> com = config["centerOfMass"];
+  std::array<double, 9> inertia = config["inertia"];
+  std::array<double, 16> transform = config["transformation"];
+
+  std::cout << "Mass:\t" << mass << std::endl;
+
+  std::cout << "COM:\t" << com[0] << "\t" << com[1] << "\t" << com[2]
+            << std::endl;
+  std::cout << "Inertia: " << std::endl;
+  for (int i = 0; i < 3; ++i) {
+    std::cout << "\t";
+    for (int j = 0; j < 3; ++j) {
+      // column-major format
+      std::cout << inertia[j * 3 + i] << "\t";
+    }
+    std::cout << std::endl;
+  }
+
+  std::cout << "Transform:\n";
+  for (int i = 0; i < 4; ++i) {
+    std::cout << "\t";
+    for (int j = 0; j < 4; ++j) {
+      // column-major format
+      std::cout << transform[j * 4 + i] << "\t";
+    }
+    std::cout << std::endl;
+  }
+
+  robot.setLoad(mass, com, inertia);
+  robot.setEE(transform);
+}
+} // namespace settings
